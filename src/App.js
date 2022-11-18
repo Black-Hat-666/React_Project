8d45dd1686b198fe22ca442325307855baaa1689
Jackson Londoño <alexisjacksonlon213@gmail.com>
2022-11-18 12:30:52 -0500

    Mi proyecto de ReactJs

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './Yin_yang.svg.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='root'>
+      <h1>Hello world!</h1>
+      <h2>Bye Group!</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+{/* <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>HOLAaaaaaaa</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */}
